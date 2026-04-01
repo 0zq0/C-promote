07146f1557446d4c7647c8fe5ff0eb3e9a03881b
zhang <2622201863@qq.com>
2026-04-01 22:08:41 +0800

    使用VS专属gitignore，清理所有编译文件

--- VS-code/滑动窗口与双指针/定长子串中元音的最大数目/main.c
+++ VS-code/滑动窗口与双指针/定长子串中元音的最大数目/main.c
@@ -1,34 +1,84 @@
 #include<stdio.h>
-
-#define Max(a,b) ((a)>(b))?(a):(b)
+#include<string.h>
+#define MAX(a,b) ((a)>(b))?(a):(b)
 
 int maxVowels(char* s, int k)
 {
-    int ans = 0;
-    int vowel = 0;
-    for (int i = 0; s[i]; i++)
-    {
-        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
-        {
-            vowel++;
-        }
-        int left = i - k + 1;
-        if (left < 0)
-        {
-            continue;
-        }
-        ans = Max(ans, vowel);
-        char out = s[left];
-        if (out == 'a' || out == 'e' || out == 'i' || out == 'o' || out == 'u')
-        {
-            vowel--;
-        }
-    }
-    return ans;
+	int len = (int) strlen(s);
+	int vowel = 0;
+	int ans = 0;
+	for (int i = 0; i < len; i++)//for(int i=0;s[i];i++)
+	{
+		if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+		{
+			vowel++;
+		}
+		int left = i - k + 1;
+		if (left < 0)
+		{
+			continue;
+		}
+		ans = MAX(ans, vowel);
+		char out = s[left];
+		if (out == 'a' || out == 'e' || out == 'i' || out == 'o' || out == 'u')
+		{
+			vowel--;
+		}
+	}
+	return ans;
+}
+
+int isVowel(char c)
+{
+	return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+}
+
+int Max_Vowels(char* s, int k)
+{
+	int len = (int) strlen(s);
+	int current_count = 0;
+	int max_count = 0;
+	if (k > len)
+	{
+		return 0;
+	}
+	for (int i = 0; i < k; i++)
+	{
+		if (isVowel(s[i]))
+			current_count++;
+	}
+	max_count = MAX(max_count, current_count);
+	for (int i = k; i < s[i]; i++)
+	{
+		if (isVowel(s[i]))
+		{
+			current_count++;
+		}
+		else
+		{
+			current_count--;
+		}
+
+	}
+	max_count = MAX(max_count, current_count);
+	return max_count;
+
 }
 
 int main()
 {
-    printf("hello\n");
-    return 0;
+	char arr[] = "abciiidef";
+	char* s = "leetcode";
+	int k = 0;
+	scanf_s("%d", &k);
+	int ret1 = 0;
+	int ret2 = 0;
+	ret1 = Max_Vowels(arr, k);
+	ret2 = Max_Vowels(s, k);
+	printf("数组arr中的元音最大数目是：%d\n", ret1);
+	printf("字符串s中的元音最大数目是：%d\n", ret2);
+
+	return 0;
+
 }
+
